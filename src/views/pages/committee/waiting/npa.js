3bc65c1ce59be8894a33b9864a86e2177a5b44a7
Chaimongkol <cpanawasri@gmail.com>
2025-04-20 22:21:27 +0700

    add route

---
+++ src/views/pages/committee/waiting/npa.js
@@ -0,0 +1,36 @@
+import { useEffect, useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getUserData } from "@utils";
+import According from "@views/components/panel/according";
+import { 
+  importClassify
+} from "@services/api";
+
+const user = getUserData();
+const NPA = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="content">
+        <h4 className="mb-3">สาขาเสนอ NPA</h4>
+        <div className="mt-4">
+          <div className="row g-4">
+            <div className="col-12 col-xl-12 order-1 order-xl-0">
+              <div className="mb-9">
+                <According 
+                  title={'-'}
+                  className={"my-4"}
+                  children={(
+                    <>
+                    </>
+                  )}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default NPA;
