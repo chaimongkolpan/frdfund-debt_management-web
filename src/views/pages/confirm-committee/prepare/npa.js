dd43c860404fda374a58601dec5fec36c7517f33
Chaimongkol <cpanawasri@gmail.com>
2025-12-03 14:36:11 +0700

    fix submit confirm

--- src/views/pages/confirm-committee/prepare/npa.js
+++ src/views/pages/confirm-committee/prepare/npa.js
@@ -50,21 +50,21 @@ const NPA = () => {
   const onSubmit = async () => {
     const form = new FormData();
     if (files && files.length > 0) {
-      await new Promise((resolve, reject) => {
-        try {
-          const param = requestApproveData.map((item,index) => {
-            form.append('ids[' + index + ']', item.id_debt_confirm.toString());
-            return {
-              id_debt_confirm: item.id_debt_confirm
-            }
-          });
-          resolve(param);
-        } catch {
-          reject(null);
-        }
-      });
       files.forEach((item) => form.append("files", item));
     }
+    await new Promise((resolve, reject) => {
+      try {
+        const param = requestApproveData.map((item,index) => {
+          form.append('ids[' + index + ']', item.id_debt_confirm.toString());
+          return {
+            id_debt_confirm: item.id_debt_confirm
+          }
+        });
+        resolve(param);
+      } catch {
+        reject(null);
+      }
+    });
     form.append("debt_manage_type", 'NPA')
     form.append("branch_correspondence_no", 'กฟก '+ getBookNo()  + branchNo)
     form.append("branch_correspondencel_date", stringToDateTh(branchDate, false))
--- src/views/pages/confirm-committee/prepare/npl.js
+++ src/views/pages/confirm-committee/prepare/npl.js
@@ -49,21 +49,21 @@ const NPL = () => {
   const onSubmit = async () => {
     const form = new FormData();
     if (files && files.length > 0) {
-      await new Promise((resolve, reject) => {
-        try {
-          const param = requestApproveData.map((item,index) => {
-            form.append('ids[' + index + ']', item.id_debt_confirm.toString());
-            return {
-              id_debt_confirm: item.id_debt_confirm
-            }
-          });
-          resolve(param);
-        } catch {
-          reject(null);
-        }
-      });
       files.forEach((item) => form.append("files", item));
     }
+    await new Promise((resolve, reject) => {
+      try {
+        const param = requestApproveData.map((item,index) => {
+          form.append('ids[' + index + ']', item.id_debt_confirm.toString());
+          return {
+            id_debt_confirm: item.id_debt_confirm
+          }
+        });
+        resolve(param);
+      } catch {
+        reject(null);
+      }
+    });
     form.append("debt_manage_type", 'NPL')
     form.append("branch_correspondence_no", 'กฟก '+ getBookNo()  + branchNo)
     form.append("branch_correspondencel_date", stringToDateTh(branchDate, false))
