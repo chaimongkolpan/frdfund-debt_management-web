1759d46c982cb0da44debdcde3ff9fbe148ef2f9
Chaimongkol <cpanawasri@gmail.com>
2025-12-10 02:49:40 +0700

    fix policy

--- src/views/components/proposeForApproval/NPL/commercialBanksOrLegalEntitiesTable.js
+++ src/views/components/proposeForApproval/NPL/commercialBanksOrLegalEntitiesTable.js
@@ -38,7 +38,7 @@ const CommercialBanksOrLegalEntitiesTable = ({
                   className="form-check-input"
                   type="checkbox"
                   checked={checked}
-                  onChange={() => handleSelect(item.id_debt_register, index)}
+                  onChange={() => handleSelect(item.id_debt_management, index)}
                 />
               </div>
             </td>
