62dc2ce8f4060883c55265b82b98c14fb1bf6b2e
Chaimongkol <cpanawasri@gmail.com>
2026-02-24 21:54:05 +0700

    fix: card

--- src/views/pages/legal-contract/index.js
+++ src/views/pages/legal-contract/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Nav, NavItem, NavLink, TabContent, TabPane, Spinner } from 'reactstrap'
-import { getUserData } from "@utils";
+import { getUserData, toCurrency } from "@utils";
 import According from "@views/components/panel/according";
 import Dropdown from "@views/components/input/DropdownSearch";
 import Modal from "@views/components/modal/customModal";
